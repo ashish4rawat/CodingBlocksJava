d2c7c77178c9ebe9feed8c038038a659cd90a6c4
ashish <ashish4rawat@gmail.com>
2018-10-10 14:20:18 +0530

    first commit

---
+++ src/com/coding/Hu.java
@@ -0,0 +1,13 @@
+package com.coding;
+
+import java.util.Arrays;
+
+public class Hu {
+    public static void main(String[] args) {
+        String st = " 1 2 3 3 4";
+        System.out.println(Arrays.toString(st.split(" ")));
+        for (int i = 1; i <st.length() ; i++) {
+
+        }
+    }
+}
